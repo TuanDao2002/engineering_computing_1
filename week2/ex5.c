c4b3d6c362eefae0e1449fb324ebbd86bc09a165
Tuan Dao Kha <s3877347@s00204027-L0741.RMIT.EDU.VN>
2022-03-09 15:19:26 +0700

    add comment and refactor

--- week2/ex5.c
+++ week2/ex5.c
@@ -5,7 +5,7 @@ int main(){
     printf("Enter the number of minutes: ");
     scanf("%d", &minutes);
     hours = minutes / 60;
-    minutes = minutes % 60;
+    minutes %= 60;
     printf("The result is: %d hours %d minutes", hours, minutes);
     return 0;
 }
--- week2/ex6.c
+++ week2/ex6.c
@@ -5,13 +5,16 @@ int main(){
     int i;
     printf("Enter an interger: ");
     scanf("%d", &i);
-    while (i / 10 != 0) {
-        sum += i % 10;
-        i = i / 10;
-    }
+    
+    // not use while in this exercise
+    sum += i % 10;
+    i = i / 10;
 
-    sum += i;
+    sum += i % 10;
+    i = i / 10;
 
+    sum += i % 10;
+    i = i / 10;
     printf("The sum of all the digits is: %d", sum);
     return 0;
 }
