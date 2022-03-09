f473f1a5b293175a0163e1a1e60487b8186b5c2b
Tuan Dao Kha <s3877347@s00204027-L0741.RMIT.EDU.VN>
2022-03-09 15:40:51 +0700

    add comment

--- week2/ex4.c
+++ week2/ex4.c
@@ -7,6 +7,7 @@ int main(){
     printf("Enter 3 assessment scores: ");
     scanf("%d %d %d", &asmt1, &asmt2, &asmt3);
 
+    // use float
     float final_result = (asmt1 * asmt1_weight + asmt2 * asmt2_weight + asmt3 * asmt3_weight) / 100.00;
 
     printf("The final result is: %.2f", final_result);
