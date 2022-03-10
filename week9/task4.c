d2cf8a227b0a55185ebabf9801631ee9264c0308
TuanDao2002 <s3877347@rmit.edu.vn>
2022-03-10 10:53:49 +0700

    add new file

--- week9/task4.c
+++ week9/task4.c
@@ -18,8 +18,32 @@ void printarrayelements(int* arr, int size) {
     printf("%d ", arr[size - 1]);
 }
 
-char* inttostring(int num, char* s) {
-   
+// this method require a char* temp to function
+char* convert2string2(int num, char* s) {
+    if (num == 0) {
+        return s;
+    }
+
+    char* current_char = s;
+    char* next_char = convert2string2(num / 10, current_char);
+    
+    *next_char = num % 10 + '0';
+    next_char++;
+    *next_char = '\0'; // terminate the string with '\0'
+
+    return next_char;
+}
+
+void convert2string(int num, char* s) {
+    static int index = 0;
+    if (num == 0) {
+        return;
+    }
+
+    convert2string(num / 10, s);
+    s[index] = num % 10 + '0';
+    index++;
+    s[index] = '\0';
 }
 
 int main(){
@@ -31,5 +55,9 @@ int main(){
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     printarrayelements(arr, sizeof(arr) / sizeof(int));
     printf("\n");
+
+    char int_str[20];
+    convert2string(n, int_str);
+    printf("Print out integer as string: %s\n", int_str);
     return 0;
 }
