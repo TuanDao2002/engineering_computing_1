a27e7f9599909460792f0fb4f9dc0972b9ed4cfd
Tuan Dao Kha <s3877347@iMac.rmit.edu.vn>
2022-03-16 15:40:22 +0700

    refactor

--- week3/ex2.c
+++ week3/ex2.c
@@ -5,12 +5,12 @@ int main() {
     do {
         printf("Enter a positive number: ");
         scanf("%d", &num);
-        if (num < 0) {
-            break;
+        if (num > 0) {
+            sum += num;
+            count++;
         }
-        sum += num;
-        count++;
-    } while(num >= 0);
+       
+    } while(num > 0);
 
     printf("Sum is %d\n", sum);
     printf("There are %d positive integers\n", count);
