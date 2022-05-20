61369098ff12558b6e7d24bae157ce0435ab1d1d
TuanDao2002 <s3877347@rmit.edu.vn>
2022-05-20 14:51:41 +0700

    update

---
+++ Lab Assessment 3/question3.c
@@ -0,0 +1,16 @@
+/*
+    RMIT University Vietnam
+    Course: EEET2601 Engineering Computing 1
+    Semester: 2021C
+    Lab Assessment: 3
+    Author: Dao Kha Tuan
+    ID: s3877347
+    Compiler used: 8.1.0
+    Created  date: 20/05/2022
+    Acknowledgement: 'None'
+*/
+
+int main() {
+
+    return 0;
+}
